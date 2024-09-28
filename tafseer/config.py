513b215295380d7fcd96450820423a699b6eca58
Moataz-AbdElwadood <moatazelwadood@gmail.com>
2024-09-28 18:58:22 +0300

    Delete tafseer directory

--- tafseer/config.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-
-
-class GPTConfig:
-    GPT_API_KEY = settings.OPENAI_API_KEY
--- tafseer/tab/config.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-
-class TapConfig:
-    API_BASE_URL = settings.TAP_GATEWAY_BASE_URL
-    API_KEY = settings.TAP_GATEWAY_SECRET_KEY
